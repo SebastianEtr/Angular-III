26e63e4dd863acd1ab8cdd136b5acbfbe1601bdb
sebaseter <sebaslopezz654@gmail.com>
2022-10-26 16:39:37 -0500

    newCountry

--- proyecto-real/src/app/app.module.ts
+++ proyecto-real/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CountryListComponent } from './components/country-list/country-list.component';
 import { InterceptorService } from './interceptors/interceptor.service';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     {
